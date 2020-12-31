30c8337c89f0e3c27fd91e985dd765aae4164538
gouf <innocent.zero@gmail.com>
2020-12-31 10:28:24 +0900

    定数定義を別ファイルに移動

--- lib/sunrise_sunset_japan/scraper.rb
+++ lib/sunrise_sunset_japan/scraper.rb
@@ -3,12 +3,11 @@
 require 'mechanize'
 require 'ostruct'
 require 'date'
+require File.join(__dir__, 'scraper', 'constants')
 
 class SunriseSunsetJapan
   class Scraper
     attr_reader :sunrise_sunset_list, :target_date
-
-    BASE_URL = 'https://motohasi.net/SunriseSunset/JapanSun.php'
 
     TABLE_DATA =
       Struct.new(
@@ -18,64 +17,16 @@ class SunriseSunsetJapan
         :sunset,
       )
 
-    TARGET_PREFECTURES = %w[
-      北海道
-      青森県
-      岩手県
-      宮城県
-      秋田県
-      山形県
-      福島県
-      茨城県
-      栃木県
-      群馬県
-      埼玉県
-      千葉県
-      東京都
-      神奈川
-      新潟県
-      富山県
-      石川県
-      福井県
-      山梨県
-      長野県
-      岐阜県
-      静岡県
-      愛知県
-      三重県
-      滋賀県
-      京都府
-      大阪府
-      兵庫県
-      奈良県
-      和歌山
-      鳥取県
-      島根県
-      岡山県
-      広島県
-      山口県
-      徳島県
-      香川県
-      愛媛県
-      高知県
-      福岡県
-      佐賀県
-      長崎県
-      熊本県
-      大分県
-      宮崎県
-      鹿児島
-      沖縄県
-    ].freeze
-
     def initialize(target_date)
       @target_date = target_date
 
-      page = Mechanize.new.get("#{BASE_URL}?TargetDate=#{target_date}&PCMode=")
+      page =
+        Mechanize.new
+                 .get("#{Constants::BASE_URL}?TargetDate=#{target_date}&PCMode=")
 
       prefecture_elements =
         page.search('.MainTable td')
-            .find_all { |td| TARGET_PREFECTURES.include?(td.text) }
+            .find_all { |td| Constants::TARGET_PREFECTURES.include?(td.text) }
 
       @sunrise_sunset_list =
         prefecture_elements.map do |prefecture_element|
